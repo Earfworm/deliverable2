d240cca4b144e3e9c0d7cc352be3bc4ab7c4077a
deshawn <deshawnreid2021@gmail.com>
2022-09-04 03:50:14 -0400

    Html and JS update

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let userName = prompt("Welcome to GC mini golf! What is your name?");
-let gameChoice = prompt( `Hi ${userName}! Would you like to play 3 or 6 rounds? `);
+let rounds = prompt( `Hi ${userName}! Would you like to play 3 or 6 rounds? `);
 let hole = 1;
 let total = 0;
 let par = 0;
 
 
-if (Number(gameChoice) === 3) {
-    for(i = 0; i < Number(gameChoice); i++){
+if (Number(rounds) === 3) {
+    for(i = 0; i < Number(rounds); i++){
     let putts = prompt(`How many putts for hole ${hole}? (par is 3)`);
      hole = hole + 1 ;
-    console.log(`Total Putts: ${total += Number(putts)}`);
+    console.log(`Total Putts After Round: ${total += Number(putts)}`);
     }
     par = total - 9;
-    } else if (Number(gameChoice) === 6){
+    } else if (Number(rounds) === 6){
 
-    for(i = 0; i < Number(gameChoice); i++){
+    for(i = 0; i < Number(rounds); i++){
         let putts = prompt(`How many putts for hole ${hole}? (par is 6)`);
         hole = hole + 1;
-      console.log(`Total Putts: ${total += Number(putts)}`);
+      console.log(`Total Putts After Round: ${total += Number(putts)}`);
     }
     par = total - 18;
     } else {
